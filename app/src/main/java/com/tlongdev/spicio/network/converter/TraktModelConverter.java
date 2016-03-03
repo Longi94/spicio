52dee0143b5da7311fd5a610617857d29d65ecd1
Long Tran <lngtrn94@gmail.com>
2016-03-03 10:43:45 +0100

    convert image objects

--- app/src/main/java/com/tlongdev/spicio/network/converter/TraktModelConverter.java
+++ app/src/main/java/com/tlongdev/spicio/network/converter/TraktModelConverter.java
@@ -1,9 +1,45 @@
 package com.tlongdev.spicio.network.converter;
 
+import com.tlongdev.spicio.domain.model.Image;
+import com.tlongdev.spicio.domain.model.Images;
+import com.tlongdev.spicio.network.model.TraktImage;
+import com.tlongdev.spicio.network.model.TraktImages;
+
 /**
  * @author Long
  * @since 2016. 03. 02.
  */
 public class TraktModelConverter {
 
+    public static Image convertToImage(TraktImage traktImage) {
+        if (traktImage == null) {
+            return null;
+        }
+
+        Image image = new Image();
+
+        image.setFull(traktImage.getFull());
+        image.setMedium(traktImage.getMedium());
+        image.setThumb(traktImage.getThumb());
+
+        return image;
+    }
+
+    public static Images convertToImage(TraktImages traktImages) {
+        if (traktImages == null) {
+            return null;
+        }
+
+        Images images = new Images();
+
+        images.setBanner(convertToImage(traktImages.getBanner()));
+        images.setClearArt(convertToImage(traktImages.getClearart()));
+        images.setFanArt(convertToImage(traktImages.getFanart()));
+        images.setLogo(convertToImage(traktImages.getLogo()));
+        images.setPoster(convertToImage(traktImages.getPoster()));
+        images.setScreenshot(convertToImage(traktImages.getScreenshot()));
+        images.setThumb(convertToImage(traktImages.getThumb()));
+
+        return images;
+    }
 }
