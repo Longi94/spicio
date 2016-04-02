0f1e4538d6e081ab81664e3f4e162bd650c73d1d
Long Tran <lngtrn94@gmail.com>
2016-04-02 20:04:47 +0200

    initial activity types

---
+++ app/src/main/java/com/tlongdev/spicio/domain/model/ActivityType.java
@@ -0,0 +1,22 @@
+package com.tlongdev.spicio.domain.model;
+
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author longi
+ * @since 2016.04.02.
+ */
+public abstract class ActivityType {
+
+    @IntDef({WATCHED, SKIPPED, LIKED, BECAME_FRIENDS})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Enum {}
+
+    public static final int WATCHED = 0;
+    public static final int SKIPPED = 1;
+    public static final int LIKED = 2;
+    public static final int BECAME_FRIENDS = 3;
+}
